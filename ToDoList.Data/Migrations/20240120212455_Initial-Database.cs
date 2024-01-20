900676c3764f0ffb7d37a3e8a3e7d881ad579455
Kostiantyn Bulgaka <kostyabulgaka3@gmail.com>
2024-01-20 23:26:36 +0200

    Implemented DB functionality

---
+++ ToDoList.Data/Migrations/20240120212455_Initial-Database.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ToDoList.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "items",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    IsCompleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_items", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "items");
+        }
+    }
+}
---
+++ ToDoList.Data/ToDoDataContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.Data.Interfaces;
+
+namespace ToDoList.Data
+{
+    public class ToDoDataContext : DbContext, IToDoDataContext
+    {
+        public ToDoDataContext() :base() { }
+
+        public ToDoDataContext(DbContextOptions<ToDoDataContext> options) : base(options) 
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured){
+                optionsBuilder.UseNpgsql("Server=localhost;Database=ToDoDB;Port=5432;User Id=postgres;Password=l'horizon");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+
+        public DbSet<ToDoItem> items { get; set; }
+
+        public List<ToDoItem> GetItems() 
+        {
+            return items.ToList();
+        }
+
+        public int SaveChanges()
+        {
+            return base.SaveChanges();
+        }
+    } 
+}
